b31818b1260124f428397a6484429ce74cdd5c09
Chris M <webmaster@magnaws.com>
2017-12-12 09:00:39 -0500

    These shouldn't be tracked.

--- Pods/Target Support Files/BMLTiOSLib/BMLTiOSLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BMLTiOSLib : NSObject
-@end
-@implementation PodsDummy_BMLTiOSLib
-@end
--- Pods/Target Support Files/Pods-Quick NA Meeting Finder/Pods-Quick NA Meeting Finder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quick_NA_Meeting_Finder : NSObject
-@end
-@implementation PodsDummy_Pods_Quick_NA_Meeting_Finder
-@end
